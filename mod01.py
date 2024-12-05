106b241b691c2e53f1217b2fd109e8fad25cd299
Tommy H. Yeargin, Jr <88904457+yeargin2021@users.noreply.github.com>
2024-12-05 02:43:26 -0500

    commit

---
+++ mod01.py
@@ -0,0 +1,6 @@
+try:
+    print("This is a try statement.") # Note the error. This will not execute at runtime.
+except:
+    print("This is an exception.") # This will execute at runtime.
+else:
+    print(2+2)
---
+++ mod02.py
@@ -0,0 +1,9 @@
+try:
+    passe
+
+#     i = 1
+#     while i < 10:
+#         print(i)
+#         i += 1
+except Exception as e:
+    print(f"There is an error: {e}")
